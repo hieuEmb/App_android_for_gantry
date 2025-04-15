7def4d87e58959d734e554f941b2c51bddcaf9ee
hieuEmb <hieu0353777534@gmail.com>
2025-04-15 11:28:35 +0700

    App_V18

--- App_android_for_gantry/Pages/ImportMode/ImportModePage.xaml.cs
+++ App_android_for_gantry/Pages/ImportMode/ImportModePage.xaml.cs
@@ -1,40 +1,24 @@
 namespace App_android_for_gantry.Pages.ImportMode;
+using App_android_for_gantry.ViewModels;
+using Microsoft.Maui.Networking;
+using System.ComponentModel;
+using System.Diagnostics;
 using App_android_for_gantry.Services;
 public partial class ImportModePage : ContentPage
 {
 	public ImportModePage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = ViewModel;
+    }
     private ModbusService _modbusService = new ModbusService();
+    public MainViewModel ViewModel { get; set; } = new MainViewModel();
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
-        await _modbusService.ConnectPLCAsync();     
+        await _modbusService.ConnectPLCAsync();
+        ViewModel.StartReadingMedicine();
     }
 
-    // Write
-    private async void Write_System(object sender, EventArgs e)
-    {
-        // Ki?m tra xem Entry có d? li?u không
-        if (ushort.TryParse(entryValue.Text, out ushort value))
-        {
-            try
-            {
-                System.Diagnostics.Debug.WriteLine($"value: {value}");
-                // Ghi giá tr? vào thanh ghi Modbus
-                await _modbusService.WriteHoldingRegisterAsync(1, 166, value);
-                await DisplayAlert("Thành công", $"?ã ghi {value} vào thanh ghi MW0", "OK");
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("L?i", $"Không th? ghi d? li?u: {ex.Message}", "OK");
-            }
-        }
-        else
-        {
-            await DisplayAlert("L?i", "Vui lòng nh?p s? h?p l?!", "OK");
-        }
-    }
 }
